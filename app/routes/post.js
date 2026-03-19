d7a3c31b876345913acf754bedf3098adce990c9
APrem-7 <ap7leo@gmail.com>
2026-03-19 20:59:29 +0530

    Feat:Initilazed the post route

---
+++ app/routes/post.js
@@ -0,0 +1,8 @@
+//this is going to handle all the posting stuff in my system like everything that i will be needing to do to post put stuff etc
+import { Router } from "express"
+
+const putRouter = Router()
+
+putRouter.post("/put",(req,res)=>{
+    //here we are going to put the data into the mongoDB file
+})
