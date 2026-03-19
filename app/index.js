dfd2fa0f139843c00f42b5dcf621b2fefebe1b0b
APrem-7 <ap7leo@gmail.com>
2026-03-19 19:51:05 +0530

    feat: Implement a basic Express server with a "Hello World!" endpoint.

---
+++ app/index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+
+//This is the get Hello world request
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
