632e7f7f0e2d328a9dfb5ff33e1a2c956116bf30
APrem-7 <ap7leo@gmail.com>
2026-03-20 19:39:57 +0530

    feat: Implement MongoDB connection utility and integrate it into the application, adopting ES module syntax.

--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
-require("dotenv").config();
-const express = require("express");
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
